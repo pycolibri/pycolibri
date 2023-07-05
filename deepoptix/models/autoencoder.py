8395623863bd83f55d4e69a466d8ec81598ba27e
bemc22 <brayansneider22@gmail.com>
2023-07-05 08:19:48 -0500

    fix wrong library

--- deepoptix/models/autoencoder.py
+++ deepoptix/models/autoencoder.py
@@ -1,16 +1,16 @@
 """ Autoencoder Architecture """
 
-import torch.nn as nn
+import tensorflow as tf
 
 # TODO: Implement Autoencoder model
-def autoencoder(in_channels: int, out_channels: int) -> nn.Module:
+def autoencoder(in_channels: int, out_channels: int) -> tf.keras.Model:
     """
     Create a Autoencoder model
 
     :param in_channels: number of input channels
     :param out_channels: number of output channels
     :return: Autoencoder model
-    :rtype: nn.Module
+    :rtype: tf.keras.Model
     """
 
     pass
--- deepoptix/models/unet.py
+++ deepoptix/models/unet.py
@@ -1,16 +1,16 @@
 """ Unet Architecture """
 
-import torch.nn as nn
+import tensorflow as tf
 
 # TODO: Implement Unet model
-def unet(in_channels: int, out_channels: int) -> nn.Module:
+def unet(in_channels: int, out_channels: int) -> tf.keras.Model:
     """
     Create a Unet model
 
     :param in_channels: number of input channels
     :param out_channels: number of output channels
     :return: Unet model
-    :rtype: nn.Module
+    :rtype: tf.keras.Model
     """
 
     pass
