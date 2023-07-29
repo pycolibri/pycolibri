2cc5f321ae02f7801aee13aa9600f687b0ce5b1a
Enmartz <Enmartz@hotmail.com>
2023-07-29 15:23:17 -0500

    updated cassi

--- deepoptix/optics/cassi.py
+++ deepoptix/optics/cassi.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
+
 class CASSI(tf.keras.layers.Layer):
-    def __init__(self, trainable=False, ca_regularizer = None, initial_ca = None, seed=None):
+    def __init__(self, trainable=False, ca_regularizer=None, initial_ca=None, seed=None):
         """
         Layer that performs the forward and transpose operator of coded aperture snapshot spectral imager (CASSI), more information refer to: Compressive Coded Aperture Spectral Imaging: An Introduction: https://doi.org/10.1109/MSP.2013.2278763
         :param trainable: Boolean, if True the coded aperture is trainable
@@ -18,7 +19,7 @@ class CASSI(tf.keras.layers.Layer):
 
     def build(self, input_shape):
         super(CASSI, self).build(input_shape)
-        self.M, self.N, self.L = input_shape # Extract spectral image shape
+        self.M, self.N, self.L = input_shape  # Extract spectral image shape
 
         if self.initial_ca is None:
             initializer = tf.random_uniform_initializer(minval=0, maxval=1, seed=self.seed)
@@ -26,41 +27,37 @@ class CASSI(tf.keras.layers.Layer):
             assert self.initial_ca.shape != (1, self.M, self.N, 1), "the start CA shape should be (1, M, N, 1)"
             initializer = tf.constant_initializer(self.initial_ca)
 
-
-        
-
         self.ca = self.add_weight(name='coded_apertures', shape=(1, self.M, self.N, 1), initializer=initializer,
-                                    trainable=self.trainable, regularizer = self.ca_regularizer)
+                                  trainable=self.trainable, regularizer=self.ca_regularizer)
 
-    def get_measurement(self, x):
-        y1 = tf.multiply(x, self.ca) # Multiplication of the scene by the coded aperture 
+    def forward(self, x, ca=None):  # perform H * x
+        ca = self.ca if ca is None else ca
+        y1 = tf.multiply(x, ca)  # Multiplication of the scene by the coded aperture
 
         # shift and sum
-        y2 = tf.zeros((1, self.M, self.N + self.L - 1, 1)) # Variable that will serve as the measurement
+        y2 = tf.zeros((1, self.M, self.N + self.L - 1, 1))  # Variable that will serve as the measurement
         for l in range(self.L):
             # Shifting produced by the prism 
             y2 += tf.pad(y1[..., l, None], [(0, 0), (0, 0), (l, self.L - l - 1), (0, 0)])
 
         return y2
 
-    def get_transpose(self, y):
-        x = tf.concat([y[..., l:l + self.N, :] for l in range(self.L)], axis=-1) # Undo unshifting and create cube version of measurement
-        return tf.multiply(x, self.ca)
+    def backward(self, y, ca=None):  # perform H^T * y
+        ca = self.ca if ca is None else ca
+        x = tf.concat([y[..., l:l + self.N, :] for l in range(self.L)],
+                      axis=-1)  # Undo unshifting and create cube version of measurement
+        return tf.multiply(x, ca)
 
-    def __call__(self, x, only_measurement=False, only_transpose=False):
+    def forward_backward(self, x, ca=None):  # perform H^T * H * x
+        ca = self.ca if ca is None else ca
+        return self.backward(self.forward(x, ca), ca)
 
-        if only_measurement:
-            return self.get_measurement(x)
+    def backward_forward(self, y, ca=None):  # perform H * H^T * y
+        ca = self.ca if ca is None else ca
+        return self.forward(self.backward(y, ca), ca)
 
-        if only_transpose:
-            return self.get_transpose(x)
-
-        return self.get_transpose(self.get_measurement(x))
-    
 
 if __name__ == "__main__":
-
-
     import matplotlib.pyplot as plt
     import tensorflow as tf
     import scipy.io as sio
@@ -68,8 +65,8 @@ if __name__ == "__main__":
 
     # load a mat file
 
-    cube = sio.loadmat(os.path.join('examples', 'data', 'spectral_image.mat'))['img']
-    
+    cube = sio.loadmat(os.path.join('examples', 'data', 'spectral_image.mat'))['img']  # (M, N, L)
+    ca = np.random.rand(1, cube.shape[0], cube.shape[1], 1)  # custom ca (1, M, N, 1)
 
     # load optical encoder
 
@@ -79,18 +76,17 @@ if __name__ == "__main__":
     # encode the cube
 
     cube_tf = tf.convert_to_tensor(cube)[None]  # None add a new dimension
-    measurement = cassi(cube_tf, only_measurement=True)
-    transpose = cassi(measurement, only_transpose=True)
-    direct_transpose = cassi(cube_tf)
-    measurement2 = cassi(transpose, only_measurement=True)
+    measurement = cassi.forward(cube_tf, ca)
+    transpose = cassi.backward(measurement, ca)
+    direct_transpose = cassi.forward_backward(cube_tf, ca)
+    measurement2 = cassi.forward(transpose, ca)
 
-
-    #Print information about tensors
+    # Print information about tensors
 
     print('cube shape: ', cube_tf.shape)
     print('measurement shape: ', measurement.shape)
     print('transpose shape: ', transpose.shape)
-    
+
     # visualize the measurement
 
     plt.figure(figsize=(10, 10))
@@ -113,4 +109,3 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
-    
