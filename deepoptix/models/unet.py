76e32c64c05f2efb350aff4eb2e6f4b09e9a30a0
bemc22 <brayansneider22@gmail.com>
2023-08-06 07:01:42 -0500

    update unet model 1. convert unet Model to Layer 2. wrapp Unet Layer with create function 3. remove Sequential Layer

--- deepoptix/models/unet.py
+++ deepoptix/models/unet.py
@@ -3,38 +3,31 @@
 import tensorflow as tf
 import layers as custom_layers
 
-class Unet(tf.keras.Model):
+class Unet(tf.keras.layers.Layer):
     """
-    Unet model
+    Unet Layer
 
-
-    :param in_channels: number of input channels
     :param out_channels: number of output channels
     :param in_features: number of input features
     :param levels: number of levels in the Unet
     :param last_activation: activation function for the last layer
-    :param size: size of the input image
+
     :return: Unet model
-    :rtype: tf.keras.Model
+    :rtype: tf.keras.Layer
     """
 
 
     def __init__(self, 
-                 in_channels, 
                  out_channels, 
                  in_features=32, 
                  levels=4, 
-                 last_activation='sigmoid',
-                 size=None):
+                 last_activation='sigmoid'):
         
         super(Unet, self).__init__()
 
         features = [in_features * 2**i for i in range(levels)]
 
-        self.inc = tf.keras.Sequential([
-            tf.keras.layers.InputLayer(input_shape=(size, size, in_channels)),
-            custom_layers.convBlock(features[0]),
-        ])
+        self.inc = custom_layers.convBlock(features[0]) 
 
         self.downs = [
             custom_layers.downBlock(features[i+1]) for i in range(levels-2)
@@ -70,6 +63,15 @@ class Unet(tf.keras.Model):
         return self.outc(x)
 
 
+def unet(in_channels, out_channels, size=None):
+
+    _input = tf.keras.Input(shape=(size, size, in_channels))
+    output = Unet(out_channels)(_input)
+    model = tf.keras.Model(inputs=_input, outputs=output)
+
+    return model
+
+
 if __name__ == "__main__":
 
     import numpy as np
@@ -78,7 +80,7 @@ if __name__ == "__main__":
     in_channels = 16
     out_channels = 1
 
-    model = Unet(in_channels, out_channels, size=size)
+    model = unet(in_channels, out_channels, size)
 
     x = np.random.randn(1, size, size, in_channels).astype(np.float32)
     y = model(x)
