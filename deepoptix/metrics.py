299593bbc5439c58376a610e56da0d08176ac01b
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-08-23 23:00:18 -0500

    docstring in english and sam metric added

--- deepoptix/metrics.py
+++ deepoptix/metrics.py
@@ -6,93 +6,113 @@ metrics.py
 The metrics
 """
 
-def psnr(y_true, y_pred, max_val):
-  """Calcula el Peak Signal to Noise Ratio entre y_true y y_pred.
+def psnr(y_true, y_pred):
+    """Calculate Peak Signal to Noise Ratio between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.  
-    max_val: Valor máximo de los tensores.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    PSNR entre y_true y y_pred.
-  """
-  return tf.image.psnr(y_true, y_pred, max_val)
+    Returns:
+        PSNR between y_true and y_pred.
+    """
+    return tf.image.psnr(y_true, y_pred, max_val=1.0)
 
-def ssim(y_true, y_pred, max_val):
-  """Calcula el Structural Similarity Index entre y_true y y_pred.
+def ssim(y_true, y_pred):
+    """Calculate Structural Similarity Index between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
-    max_val: Valor máximo de los tensores.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    SSIM entre y_true y y_pred.
-  """
-  return tf.image.ssim(y_true, y_pred, max_val)
+    Returns:
+        SSIM between y_true and y_pred.
+    """
+    return tf.image.ssim(y_true, y_pred, max_val=1.0)
 
 def mse(y_true, y_pred):
-  """Calcula el Mean Squared Error entre y_true y y_pred.
+    """Calculate Mean Squared Error between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    MSE entre y_true y y_pred.
-  """
-  return tf.keras.losses.mean_squared_error(y_true, y_pred) 
+    Returns:
+        MSE between y_true and y_pred.
+    """
+    return tf.keras.losses.mean_squared_error(y_true, y_pred) 
 
 def mae(y_true, y_pred):
-  """Calcula el Mean Absolute Error entre y_true y y_pred.
+    """Calculate Mean Absolute Error between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    MAE entre y_true y y_pred.
-  """
-  return tf.keras.losses.mean_absolute_error(y_true, y_pred)
+    Returns:
+        MAE between y_true and y_pred.
+    """
+    return tf.keras.losses.mean_absolute_error(y_true, y_pred)
 
 def accuracy(y_true, y_pred, num_classes=2):
-  """Calcula la precisión entre y_true y y_pred.
+    """Calculate accuracy between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
-    num_classes: Número de clases.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
+        num_classes: Number of classes.
 
-  Returns:
-    Precisión entre y_true y y_pred.
-  """
-  if num_classes == 2:
-    return tf.keras.metrics.binary_accuracy(y_true, y_pred)
-  else:
-    return tf.keras.metrics.categorical_accuracy(y_true, y_pred)
+    Returns:
+        Accuracy between y_true and y_pred.
+    """
+    if num_classes == 2:
+        return tf.keras.metrics.binary_accuracy(y_true, y_pred)
+    else:
+        return tf.keras.metrics.categorical_accuracy(y_true, y_pred)
   
-
 def precision(y_true, y_pred):
-  """Calcula la precisión entre y_true y y_pred.
+    """Calculate precision between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    Precisión entre y_true y y_pred.
-  """
-  return tf.keras.metrics.precision(y_true, y_pred)
+    Returns:
+        Precision between y_true and y_pred.
+    """
+    return tf.keras.metrics.precision(y_true, y_pred)
 
 def recall(y_true, y_pred):
-  """Calcula la sensibilidad entre y_true y y_pred.
+    """Calculate recall between y_true and y_pred.
 
-  Args:
-    y_true: Tensor de valores reales.
-    y_pred: Tensor de valores predichos.
+    Args:
+        y_true: Tensor of actual values.
+        y_pred: Tensor of predicted values.
 
-  Returns:
-    Sensibilidad entre y_true y y_pred.
-  """
-  return tf.keras.metrics.recall(y_true, y_pred)
+    Returns:
+        Recall between y_true and y_pred.
+    """
+    return tf.keras.metrics.recall(y_true, y_pred)
+
+def SAM(org, pred, reduce=None):
+    """Calculate Spectral Angle Mapper between org and pred.
+
+    Args:
+        org: Tensor of actual values. [n, bands] 
+        pred: Tensor of predicted values. [n, bands]
+
+    Returns:
+        SAM between org and pred.
+    """
+    numerator = tf.sum(tf.math.multiply(pred, org), axis=-1)
+    denominator = tf.norm(org, ord=1, axis=-1) * tf.norm(pred, ord=1, axis=-1)
+    val = tf.clip_by_value(numerator / denominator, -1, 1)
+    angles = tf.math.acos(val)
+
+    if reduce is None:
+        return angles
+    elif reduce == 'mean':
+        return tf.reduce_mean(angles)
+    elif reduce == 'sum':
+        return tf.reduce_sum(angles)
+    
