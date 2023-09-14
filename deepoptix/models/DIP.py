1a6319f100268d6754cc649632863f3365c06d9a
Román Alejandro Jácome Carrascal <roaljaca@gmail.com>
2023-09-14 16:13:36 -0500

    fixed dip documentation

--- deepoptix/models/DIP.py
+++ deepoptix/models/DIP.py
@@ -12,49 +12,69 @@ class DIP_CASSI(tf.keras.models.Model):
     """
     DIP CASSI Model
 
+    Args:
+        input_shape (list): Shape of the input.
+        recon (str): Reconstruction network.
+        initilization (str): Type of initialization.
+        optical_layer (tf layer): Optical layer.
+        mode (str): CASSI mode.
+        network_args (dict): Arguments for the reconstruction network.
+
+    Returns:
+        tf.keras.model: DIP Model
+
     """
 
-    def __init__(self,input_shape=[128,128,25], recon='unet',initilization='random',optical_layer=None,mode='base',network_args=None,):
-        """ DIP Model
+    def __init__(self, input_shape=[128, 128, 25], recon='unet', initilization='random', optical_layer=None, mode='base', network_args=None):
+        """
+        Initializes the DIP CASSI Model.
 
         Args:
-            input_shape (list): shape of the input
-            recon (str): reconstruction network
-            initialization (str): type of initilziation
-            initial_ca (array): coded aperture
-            mode (str):  cassi mode
-            network_args (dict): args of the recon network
+            input_shape (list): Shape of the input.
+            recon (str): Reconstruction network.
+            initilization (str): Type of initialization.
+            optical_layer (tf layer): Optical layer.
+            mode (str): CASSI mode.
+            network_args (dict): Arguments for the reconstruction network.
 
-        
         Returns:
             tf.keras.model: DIP Model
-            
+
         """
-        super(DIP_CASSI,self).__init__()
+        super(DIP_CASSI, self).__init__()
         self.initilization = initilization
         self.optics = optical_layer
         self.input_size = input_shape
 
-        if recon=='unet':
-            self.recon = Unet(out_channels=input_shape[-1],features=network_args['features'],last_activation='relu')
+        if recon == 'unet':
+            self.recon = Unet(out_channels=input_shape[-1], features=network_args['features'], last_activation='relu')
         elif recon == 'autoencoder':
-             self.recon = Autoencoder(out_channels=input_shape[-1],features=network_args['features'],last_activation='relu',reduce_spatial=network_args['reduce_spatial'])   
+            self.recon = Autoencoder(out_channels=input_shape[-1], features=network_args['features'], last_activation='relu', reduce_spatial=network_args['reduce_spatial'])
         else:
             raise ValueError("Choose models autoencoder or unet")
 
-    def __call__(self,y,training=True):
-        if self.initilization=='random':
-            z0 = tf.random.normal(shape=(1,*self.input_size))
-        if self.initilization=='transpose':
-            z0 = self.optics(y,type_calculation='backward')
+    def __call__(self, y, training=True):
+        """
+        Perform forward pass.
 
-        
+        Args:
+            y: Input data.
+            training (bool): Training mode.
+
+        Returns:
+            ys: Forward pass result if training=True, else the reconstructed data (x).
+
+        """
+        if self.initilization == 'random':
+            z0 = tf.random.normal(shape=(1, *self.input_size))
+        if self.initilization == 'transpose':
+            z0 = self.optics(y, type_calculation='backward')
+
         x = self.recon(z0)
 
-        ys = self.optics(x,type_calculation='forward')
+        ys = self.optics(x, type_calculation='forward')
 
         if training:
             return ys
         else:
             return x
-        
