c2bf9d129163cc342d2b995667b79d8198523263
Roman Alejandro Jacome Carrascal <roaljaca@gmail.com>
2023-09-19 14:42:30 -0700

    Merge DIP to main (#24)
    * fix imports, remove *
    
    * modified build_network, recieve Layer and dict of params
    
    * add autoencoder in build_network
    
    * solve dependencies conflicts
    
    * update dip #16
    
    * fixed dip documentation
    
    ---------
    
    Co-authored-by: bemc22 <brayansneider22@gmail.com>

--- deepoptix/models/__init__.py
+++ deepoptix/models/__init__.py
@@ -1,39 +1,40 @@
-from unet import Unet
+from models.custom_layers import *
 
+from models.unet import Unet
+from  models.autoencoder import Autoencoder
+from models.DIP import DIP_CASSI
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 
-models = {
-    "Unet": Unet
-}
 
-__all__ = models.keys()
-
-def build_network(model_name="Unet", in_channels=1, out_channels=1, size=None, last_activation='sigmoid'):
+def build_network(model=Unet, size=256, in_channels=31 ,**network_params):
     """ Build the network model
 
     Args:
-        model_name (str, optional): Model name. Defaults to "unet".
+        model_name (tf.keras.Layer, optional): Model name. Defaults to "unet".
         in_channels (int, optional): Number of input channels. Defaults to 1.
-        out_channels (int, optional): Number of output channels. Defaults to 1.
         size (int, optional): Size of the input image. Defaults to None.
-        last_activation (str, optional): Activation function for the last layer. Defaults to 'relu'.
-
+        **network_params (dict): Network parameters
     """
-
-    if model_name not in models.keys():
-        raise ValueError(f"Model {model_name} not found. Available models: {list(models.keys())}")
-    
     _input = Input(shape=(size, size, in_channels))
-    output = models[model_name](out_channels, last_activation=last_activation)(_input)
+    output = model(**network_params)(_input)
     model = Model(inputs=_input, outputs=output)
-
     return model
     
 if __name__ == "__main__":
     import tensorflow as tf
 
-    model = build_network(model_name="Unet", in_channels=1, out_channels=1, size=32)
+
+    model_layer = Autoencoder
+
+    network_params = dict(
+        out_channels=1,
+        features=[32, 64, 128, 256],
+        last_activation='relu',
+        reduce_spatial = True
+    )
+
+    model = build_network(model=model_layer, size=32, in_channels=1, **network_params)
     model.summary()
 
     x = tf.random.normal((1, 32, 32, 1))
---
+++ deepoptix/models/autoencoder.py
@@ -0,0 +1,120 @@
+""" Autoencoder Architecture """
+
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+from tensorflow.keras.models import Model
+from . import custom_layers 
+
+
+class Autoencoder(Layer):
+    """
+    Autoencoder layer
+    """
+
+    def __init__(self, 
+                 out_channels, 
+                 features=[32, 64, 128, 256],
+                 last_activation='sigmoid',
+                 reduce_spatial = False):
+        """ Autoencoder Layer
+
+            Args:
+                out_channels (int): number of output channels
+                features (list, optional): number of features in each level of the Unet. Defaults to [32, 64, 128, 256].
+                last_activation (str, optional): activation function for the last layer. Defaults to 'sigmoid'.
+                reduce_spatial (bool): select if the autoencder reduce spatial dimension
+                
+            
+            Returns:
+                tf.keras.Layer: Autoencoder model
+                
+        """    
+        super(Autoencoder, self).__init__()
+
+        levels = len(features)
+
+        self.inc = custom_layers.convBlock(features[0], mode='CBRCBR') 
+        if reduce_spatial:
+            self.downs = [
+                custom_layers.downBlock(features[i+1]) for i in range(levels-2)
+            ]
+            self.ups = [
+                custom_layers.upBlockNoSkip(features[i] // 2) for i in range(levels-2, 0, -1)
+            ]
+            self.bottle = custom_layers.downBlock(features[-1] // 2)
+            self.ups.append(custom_layers.upBlockNoSkip(features[0] // 2))
+        else:
+            self.downs = [
+                custom_layers.convBlock(features[i+1], mode='CBRCBR') for i in range(levels-2)
+            ]
+
+            self.bottle = custom_layers.convBlock(features[-1], mode='CBRCBR')
+
+            self.ups = [
+                custom_layers.convBlock(features[i] // 2, mode='CBRCBR') for i in range(levels-2, 0, -1)
+            ]
+            self.ups.append(custom_layers.convBlock(features[0],mode='CBCBR'))
+
+            
+        self.outc = custom_layers.outBlock(out_channels, last_activation)
+
+    def call(self, inputs, get_latent=False,**kwargs):
+
+        x = self.inc(inputs)
+        
+        for down in self.downs:
+            x = down(x)
+
+        xl = self.bottle(x)
+        x = xl
+        for up in self.ups:
+            x = up(x)
+        if get_latent:
+            return self.outc(x),xl
+        else:
+            return self.outc(x)
+
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import tensorflow as tf
+    import scipy.io as sio
+    import os
+
+    # load a mat file
+
+    cube = sio.loadmat(os.path.join('deepoptix', 'examples', 'data', 'spectral_image.mat'))['img']  # (M, N, L)
+
+
+    # load optical encoder
+    cube_tf = tf.convert_to_tensor(cube)[None]  # None add a new dimension
+    print(cube_tf.shape)
+    model = Autoencoder(reduce_spatial=True,out_channels=cube_tf.shape[-1],features=[4,16,6],last_activation='relu')
+    model.build(cube_tf.shape)  # this is only for the demo
+
+    # encode the cube
+
+    recon = model(cube_tf,True)
+
+
+    # Print information about tensors
+
+
+    # visualize the measurement
+
+    plt.figure(figsize=(10, 10))
+
+    plt.subplot(131)
+    plt.title('cube')
+    plt.imshow(cube[..., 0])
+
+    plt.subplot(132)
+    plt.title('latent space')
+    plt.imshow(recon[1][0, ...])
+
+    plt.subplot(133)
+    plt.title('recon')
+    plt.imshow(recon[0][0, ..., 0])
+
+    plt.show()
