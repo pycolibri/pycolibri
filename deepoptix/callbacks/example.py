9389e00ca7fb8d6cd32296b05c63516c36947675
Paula Andrea Arguello Gutierrez <paula2191444@correo.uis.edu.co>
2023-09-20 21:07:21 -0500

    update with test

--- deepoptix/callbacks/example.py
+++ deepoptix/callbacks/example.py
@@ -1,9 +1,10 @@
-import tensorflow as tf
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import scipy.io as sio
 
+class LossAndEncoderOutputCallback(tf.keras.callbacks.Callback):
 
-class LossAndPredictionCallback(tf.keras.callbacks.Callback):
-    def __init__(self, dataset, num_samples=5, plot_graph=False):
+    def __init__(self, dataset, num_samples=1, plot_graph=False):
         """
         Callback para mostrar pérdida y predicciones durante el entrenamiento.
 
@@ -17,7 +18,8 @@ class LossAndPredictionCallback(tf.keras.callbacks.Callback):
             Indica si se deben trazar las imágenes de Ground Truth y las predicciones, por defecto es False.
 
         """
-        super(LossAndPredictionCallback, self).__init__()
+
+        super(LossAndEncoderOutputCallback, self).__init__()
         self.dataset = dataset
         self.num_samples = num_samples
         self.plot_graph = plot_graph
@@ -34,38 +36,79 @@ class LossAndPredictionCallback(tf.keras.callbacks.Callback):
             Diccionario que contiene las métricas de entrenamiento, por defecto es None.
 
         """
-        # Obtén muestras aleatorias del conjunto de datos
-        sample_indices = tf.random.uniform(
-            (self.num_samples,), maxval=len(self.dataset), dtype=tf.int32
-        )
-        sampled_images, sampled_labels = [], []
-        for index in sample_indices.numpy():
-            image, label = self.dataset[index]
-            sampled_images.append(image)
-            sampled_labels.append(label)
 
-        sampled_images = tf.stack(sampled_images)
-        sampled_labels = tf.stack(sampled_labels)
+        # Calcular el promedio de la pérdida en esta época
+        epoch_loss = logs["loss"]
+        epoch_num = epoch + 1
 
-        # Realiza predicciones en las muestras seleccionadas
-        predictions = self.model.predict(sampled_images)
+        # Imprimir la pérdida promedio de esta época
+        print('\n\n' + f'Época {epoch_num} - Pérdida Promedio: {epoch_loss:.4f}' + '\n\n')
 
-        # Dibuja las imágenes de Ground Truth y las predicciones si se requiere
-        if self.plot_graph:
-            plt.figure(figsize=(12, 6))
-            for i in range(self.num_samples):
-                plt.subplot(self.num_samples, 2, 2 * i + 1)
-                plt.imshow(sampled_images[i].numpy().squeeze(), cmap='gray')
-                plt.title('Ground Truth')
+        # Calcular la salida del codificador para las imágenes muestreadas
+        for _ in range(self.num_samples):
+            index = tf.random.uniform(
+                (), maxval=len(self.dataset), dtype=tf.int32
+            )
+            sampled_images = self.dataset[index]  # Una lista de imágenes
 
-                plt.subplot(self.num_samples, 2, 2 * i + 2)
-                plt.imshow(predictions[i].squeeze(), cmap='gray')
-                plt.title('Predicción')
+            # Procesar cada imagen individualmente
+            for sampled_image in sampled_images:
+                sampled_image = tf.reshape(sampled_image, (1, 256, 256, 28))
+                encoder_output = self.model.layers[2](sampled_image)  # Cambiar al índice correcto de la capa
 
-            plt.tight_layout()
-            plt.show()
+                # Plotear la imagen de entrada vs. la salida de la red
+                if self.plot_graph:
+                    num_bands = sampled_image.shape[3]  # Número de bandas en la imagen de entrada
 
-        # Reporta la pérdida en el registro
-        print(f'Epoch {epoch + 1} - Loss: {logs["loss"]:.4f}')
+                    # Crear una figura con subplots para cada banda de entrada y salida
+                    plt.figure(figsize=(num_bands, 2))  # Aumentamos el tamaño de la figura
+
+                    # Subplots para la entrada
+                    for band in range(num_bands):
+                        plt.subplot(2, num_bands, band + 1)
+                        plt.imshow(sampled_image[0, :, :, band].numpy(), cmap='gray')
+                        plt.title(f'In: {band + 1}')
+                        plt.axis('off')  # Eliminamos los ejes
+
+                    # Subplots para la salida
+                    for band in range(num_bands):
+                        plt.subplot(2, num_bands, num_bands + band + 1)
+                        plt.imshow(encoder_output[0, :, :, band].numpy(), cmap='gray')
+                        plt.title(f'Out: {band + 1}')
+                        plt.axis('off')  # Eliminamos los ejes
+
+                    plt.tight_layout()
+                    plt.show()
 
 
+
+
+
+img = sio.loadmat('../examples/data/spectral_image.mat')['img']
+
+img = img.reshape(-1, 256, 256, 28)
+
+dataset = [img, img]
+
+custom_callback = LossAndEncoderOutputCallback(dataset=dataset, plot_graph=True)
+
+
+input_shape = (256, 256, 28)
+
+inputs = tf.keras.layers.Input(shape=input_shape)
+x = tf.keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+x = tf.keras.layers.MaxPooling2D((2, 2), padding='same')(x)
+encoded = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+
+x = tf.keras.layers.Conv2D(128, (3, 3), activation='relu', padding='same')(encoded)
+x = tf.keras.layers.UpSampling2D((2, 2))(x)
+decoded = tf.keras.layers.Conv2D(28, (3, 3), activation='sigmoid', padding='same')(x)
+
+
+autoencoder = tf.keras.models.Model(inputs, decoded)
+
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+autoencoder.summary()
+
+autoencoder.fit(img, img, epochs=10, callbacks=[custom_callback])
