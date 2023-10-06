89c4dadc2e768c26d213c14932445431cd8807df
Brayan Monroy <brayansneider22@gmail.com>
2023-10-06 13:55:38 -0500

    incorporate unit test for models and optics with pytest (#42)
    * re-structured repo, models test
    
    * uniform autoencoder and unet syntaxis
    
    * add pytest for models
    
    * change name test_utils.py to utils.py
    
    * add test optics

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,20 +1,35 @@
-from test_utils import include_colibri
+import pytest
+from .utils import include_colibri
 include_colibri()
 
 from colibri_hdsp.models import build_network, Autoencoder, Unet
+import tensorflow as tf
 
+model_list = ["autoencoder", "unet"]
 
-if __name__ == "__main__":
-    import tensorflow as tf
+@pytest.fixture
+def imsize():
+    b = 1
+    h = 32
+    w = 32
+    c = 1
+    return b, h, w, c
 
+def choose_model(name, imsize):
 
-    model_layer = Autoencoder
-    model = build_network(model=model_layer, size=32, in_channels=1, out_channels=1)
-    model.summary()
-
-    x = tf.random.normal((1, 32, 32, 1))
-    y = model(x)
+    if name == "autoencoder":
+        model_layer = Autoencoder
+    elif name == "unet":
+        model_layer = Unet
     
-    print("input shape:", x.shape)
-    print("output shape:", y.shape)
+    model = build_network(model=model_layer, size=imsize[-2], in_channels=imsize[-1], out_channels=imsize[-1])
+    return model
 
+@pytest.mark.parametrize("model_name", model_list)
+def test_model(model_name, imsize):
+    
+    model = choose_model(model_name, imsize)
+    x = tf.random.normal(imsize)
+    y = model(x)
+
+    assert y.shape == x.shape
