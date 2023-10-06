243547207a7504b78c26754055b6c82552e07080
Enmartz <Enmartz@hotmail.com>
2023-10-06 16:05:13 -0500

    Update dataset scripts

--- colibri_hdsp/data/utils.py
+++ colibri_hdsp/data/utils.py
@@ -1,4 +1,7 @@
 import os
+from PIL import Image
+
+import numpy as np
 import tensorflow as tf
 
 
@@ -46,10 +49,19 @@ def get_all_filenames(root_directory):
     # Walk through the directory tree using os.walk
     for root, directories, files in os.walk(root_directory):
         for filename in files:
-            if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+            if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.mat')):
                 # Get the full path of the file
                 file_path = os.path.join(root, filename)
                 file_paths.append(file_path)
 
     file_paths.sort()
     return file_paths
+
+
+def load_image(filename):
+    image = Image.open(filename)
+
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
+    return np.array(image) / 255.
