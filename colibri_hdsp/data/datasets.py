c37d2db118278519b356ca6723c57589126b862d
Emmanuel Martínez <58752635+Enmartz@users.noreply.github.com>
2023-10-13 14:42:45 -0500

    Update dataset scripts (#45)
    * Update dataset scripts
    
    * Review answer for dataset scripts

--- colibri_hdsp/data/datasets.py
+++ colibri_hdsp/data/datasets.py
@@ -1,9 +1,7 @@
-import os
+import h5py
 
-import numpy as np
-from PIL import Image
-
-from colibri_hdsp.datasets.utils import *
+from spec2rgb import ColourSystem
+from utils import *
 
 BASIC_DATASETS = {
     'mnist': tf.keras.datasets.mnist,
@@ -20,12 +18,14 @@ class FolderDataset(tf.data.Dataset):
         data_path (str): path to the dataset
     """
 
-    def _generator(data_path):
+    def _generator(data_path, keys=''):
         """
         A generator function to load and preprocess images from the specified folder.
 
         Args:
             data_path (str): The path to the dataset folder.
+            keys (str): If the dataset contains .mat files, it should contain the keys related to each
+                         sample separated by commas. For arad dataset, it works 'cube'.
 
         Yields:
             np.ndarray: Preprocessed image data as NumPy arrays, normalized to the range [0, 1].
@@ -33,14 +33,25 @@ class FolderDataset(tf.data.Dataset):
         filenames = get_all_filenames(data_path.decode())
 
         for filename in filenames:
-            image = Image.open(filename)
+            if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+                yield load_image(filename)
 
-            if image.mode != 'RGB':
-                image = image.convert('RGB')
+            elif filename.lower().endswith('.mat'):
+                keys = keys[0].split(',') if isinstance(keys, list) else keys.decode().split(',')
 
-            yield np.array(image) / 255.
+                with h5py.File(str(filename), 'r') as mat:
+                    spectral_image = np.float32(np.array(mat[keys[0]]))
+                    spectral_image = np.transpose(spectral_image, (2, 1, 0))
 
-    def __new__(cls, data_path):
+                filename = filename.replace('_spectral', '_RGB')
+                image = load_image(filename.replace('.mat', '.jpg'))
+
+                yield image, spectral_image
+
+            else:
+                raise "The filename type image is unknown, please check the data_path"
+
+    def __new__(cls, data_path, keys=''):
         """
         Create a new FolderDataset instance.
 
@@ -50,16 +61,20 @@ class FolderDataset(tf.data.Dataset):
         Returns:
             tf.data.Dataset: A TensorFlow Dataset containing preprocessed image data.
         """
+        output_types = (tf.float32)
+        if 'arad' in data_path.lower():
+            output_types = (tf.float32, tf.float32)
+
         return tf.data.Dataset.from_generator(
             cls._generator,
-            output_types=(tf.float32),
-            args=(data_path,)
+            output_types=output_types,
+            args=(data_path, keys)
         )
 
 
 class Dataset:
     """
-    A class for managing and loading datasets.
+    A class for managing and loading data.
 
     Parameters:
         data_path (str or dict): Path to the dataset. If it is a string, it will load a basic dataset,
@@ -69,10 +84,10 @@ class Dataset:
         cache_dir (str): Directory to cache the dataset.
 
     Returns:
-        tuple: A tuple containing train and test datasets.
+        tuple: A tuple containing train and test data.
     """
 
-    def __init__(self, data_path, batch_size, buffer_size=3, chache_dir=''):
+    def __init__(self, data_path, batch_size, keys='', buffer_size=3, chache_dir=''):
         """
         Initialize the Dataset class.
 
@@ -80,6 +95,8 @@ class Dataset:
             data_path (str or dict): Path to the dataset. If it is a string, it will load a basic dataset,
                                      otherwise, if it is a dictionary, it will load a folder dataset.
             batch_size (int): Batch size.
+            keys (str): If the dataset contains .mat files, it should contain the keys related to each
+                         sample. For arad, we have keys=dict(spec='cube')
             buffer_size (int, optional): Buffer size for shuffling (default is 3).
             cache_dir (str, optional): Directory to cache the dataset.
         """
@@ -87,7 +104,7 @@ class Dataset:
         self.buffer_size = buffer_size
         self.chache_dir = chache_dir
 
-        # basic datasets
+        # basic data
 
         basic_datasets = list(BASIC_DATASETS.keys())
         if isinstance(data_path, str):
@@ -100,14 +117,14 @@ class Dataset:
             else:
                 raise ValueError('Dataset not supported')
 
-        # folder datasets
+        # folder data
 
         elif isinstance(data_path, dict):
             train_data_path = data_path['train']
             test_data_path = data_path['test']
 
-            train_dataset = FolderDataset(train_data_path)
-            test_dataset = FolderDataset(test_data_path)
+            train_dataset = FolderDataset(train_data_path, keys=keys)
+            test_dataset = FolderDataset(test_data_path, keys=keys)
 
         else:
             raise ValueError('Dataset not supported')
@@ -117,13 +134,13 @@ class Dataset:
 
     def load_basic_dataset(self, name):
         """
-        Load basic datasets with labels or classes.
+        Load basic data with labels or classes.
 
         Args:
             name (str): Name of the dataset (mnist, fashion_mnist, cifar10, cifar100).
 
         Returns:
-            tuple: Train and test datasets.
+            tuple: Train and test data.
         """
         print('Loading dataset: ', name)
 
@@ -133,6 +150,8 @@ class Dataset:
             raise ValueError('Dataset not supported')
 
         (x_train, y_train), (x_test, y_test) = dataset.load_data()
+        x_train = x_train / 255.
+        x_test = x_test / 255.
 
         # create dataset
 
@@ -168,32 +187,68 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
     # load dataset
+    keys = ''
     data_path = 'cifar10'
     # data_path = dict(train='/home/myusername/Datasets/processed/coco/train2017_r512',
     #                  test='/home/myusername/Datasets/processed/coco/train2017_r512')
+    # data_path = dict(train='/home/myusername/Datasets/raw/arad/Train_spectral',
+    #                  test='/home/myusername/Datasets/raw/arad/Valid_spectral')
+    # keys = 'cube'
     batch_size = 32
 
-    dataset = Dataset(data_path, batch_size)
+    dataset = Dataset(data_path, batch_size, keys=keys)
     train_dataset, test_dataset = dataset.train_dataset, dataset.test_dataset
 
     # print information about dataset
     print('train dataset: ', train_dataset)
     print('test dataset: ', test_dataset)
 
-    try:
-        # visualize dataset
+    # visualize samples
+
+    if isinstance(data_path, str):  # basic datasets
+        basic_datasets = list(BASIC_DATASETS.keys())
+        cmp_datasets = [dataset_name in data_path for dataset_name in basic_datasets]
+
+        if any(cmp_datasets):
+            # visualize dataset
+            for x, y in train_dataset.take(1):
+                plt.figure(figsize=(7, 7))
+                for i in range(9):
+                    plt.subplot(3, 3, i + 1)
+                    plt.imshow(x[i], cmap='gray')
+                    plt.title(f'class number: {y[i]}')
+                    plt.axis('off')
+
+                plt.tight_layout()
+                plt.show()
+
+    elif 'arad' in data_path['train']:  # arad dataset
+        cs_ciergb = ColourSystem(start=400, end=700, num=31)
+        RGB = cs_ciergb.get_transform_matrix()
+
         for x, y in train_dataset.take(1):
             plt.figure(figsize=(7, 7))
+            plt.suptitle('rgb samples')
             for i in range(9):
                 plt.subplot(3, 3, i + 1)
-                plt.imshow(x[i], cmap='gray')
-                plt.title(f'class number: {y[i]}')
+                plt.imshow(x[i])
                 plt.axis('off')
 
             plt.tight_layout()
             plt.show()
 
-    except Exception as e:
+            plt.figure(figsize=(7, 7))
+            plt.suptitle('mapped spectral samples')
+            for i in range(9):
+                rgb = cs_ciergb.spec_to_rgb(y[i].numpy())
+                plt.subplot(3, 3, i + 1)
+                plt.imshow(rgb)
+                plt.axis('off')
+
+            plt.tight_layout()
+            plt.show()
+
+    else:  # folder dataset
         for x in train_dataset.take(1):
             plt.figure(figsize=(7, 7))
             for i in range(9):
--- colibri_hdsp/data/utils.py
+++ colibri_hdsp/data/utils.py
@@ -1,4 +1,7 @@
 import os
+from PIL import Image
+
+import numpy as np
 import tensorflow as tf
 
 
@@ -44,12 +47,21 @@ def get_all_filenames(root_directory):
     file_paths = []
 
     # Walk through the directory tree using os.walk
-    for root, directories, files in os.walk(root_directory):
+    for root, _, files in os.walk(root_directory):
         for filename in files:
-            if filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+            if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.mat')):
                 # Get the full path of the file
                 file_path = os.path.join(root, filename)
                 file_paths.append(file_path)
 
     file_paths.sort()
     return file_paths
+
+
+def load_image(filename):
+    image = Image.open(filename)
+
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+
+    return np.array(image) / 255.
