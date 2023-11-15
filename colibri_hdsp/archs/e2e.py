217d526e5ab71808d772b32bf96f52610e5fe0df
Brayan Monroy <brayansneider22@gmail.com>
2023-11-15 10:53:57 -0500

    change sensing to optical layer

--- colibri_hdsp/archs/e2e.py
+++ colibri_hdsp/archs/e2e.py
@@ -5,22 +5,22 @@ class E2E(nn.Module):
     """
         End-to-end model for image reconstruction from compressed measurements.
     """
-    def __init__(self, sensing: nn.Module, decoder: nn.Module):
+    def __init__(self, optical_layer: nn.Module, decoder: nn.Module):
         """ End-to-end model for image reconstruction from compressed measurements.
         Args:
-            sensing (nn.Module): Sensing module.
+            optical_layer (nn.Module): Optical Layer module.
             decoder (nn.Module): Computational decoder module.
         """
 
 
         super(E2E, self).__init__()
-        self.sensing = sensing
+        self.optical_layer = optical_layer
         self.decoder = decoder
     
     def forward(self, x):
 
-        y      = self.sensing(x) # y = A(x)
-        x_init = self.sensing(y, type_calculation="backward") # x_init = A^T(y)
+        y      = self.optical_layer(x) # y = A(x)
+        x_init = self.optical_layer(y, type_calculation="backward") # x_init = A^T(y)
         x_hat  = self.decoder(x_init)                  # x_hat = R(x_init)
         return x_hat
 
