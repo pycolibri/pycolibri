c60a361bc2383a33d1a79793b5fe6c7df1eb4f42
factral <nelsonfabiancs8@gmail.com>
2024-03-01 12:28:32 -0500

    added h5py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='colibri_hdsp',
-    install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision', 'matplotlib'],
+    install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision', 'matplotlib', 'h5py'],
     extras_require={'doc': ['sphinx', 'furo', 'autodocsumm', 'sphinx_gallery', ]},
 )
