ec40d214f9037c436c997555cf804d6c1fb4d6dc
factral <nelsonfabiancs8@gmail.com>
2024-03-01 12:21:30 -0500

    added requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(
     name='colibri_hdsp',
-    extras_require={'doc': ['sphinx', 'furo', 'autodocsumm']},
+    install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision'],
+    extras_require={'doc': ['sphinx', 'furo', 'autodocsumm', 'sphinx_gallery']},
 )
