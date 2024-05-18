b669e4e5de37f9f063fa0af3e25de911d9204c91
Brayan Monroy <brayansneider22@gmail.com>
2024-05-17 21:12:08 -0500

    update with automatic tests (#130)
    * [FEAT] add run tests
    
    * [FIX] changes in test.yml
    
    * [FIX] fix name ghaction
    
    ---------
    
    Co-authored-by: Fabian *-* <nelsonfabiancs8@gmail.com>

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='colibri',
     install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision', 'matplotlib', 'h5py'],
-    extras_require={'doc': ['sphinx', 'furo', 'autodocsumm', 'sphinx_gallery', 'torch_dct']},
+    extras_require={'doc': ['sphinx', 'furo', 'autodocsumm', 'sphinx_gallery', 'torch_dct'],
+                    'test': ['pytest']}
 )
