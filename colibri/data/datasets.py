2522287c22f35ebfdf9c19e66699a085102269cf
Enmartz <Enmartz@hotmail.com>
2024-06-28 11:39:40 -0500

    Update dataset
    - builtin dataset (cifar10)

--- colibri/data/datasets.py
+++ colibri/data/datasets.py
@@ -1,4 +1,4 @@
-import os
+import matplotlib.pyplot as plt
 
 from torch.utils import data
 from torch.utils.data import Dataset
@@ -11,19 +11,6 @@ DATASET_READER = {
     'mat': D.load_mat,
     'h5': D.load_h5,
 }
-
-
-def get_filenames(path, extension, **kwargs):
-    if extension == 'builtin':
-        name = kwargs['name']
-        path = os.path.join(path, name)
-        os.makedirs(path, exist_ok=True)
-
-        return [path]
-
-    else:
-        """Return a list with the filenames of the files in the given path."""
-        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 
 class CustomDataset(Dataset):
@@ -39,19 +26,22 @@ class CustomDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
             preload (bool): If True, the dataset is preloaded in memory.
         """
-        self.dataset_filenames = get_filenames(path, extension, **dataset_vars)
+        self.dataset_filenames = D.get_filenames(path, extension, **dataset_vars)
         self.data_reader = DATASET_READER[extension]
         self.dataset_vars = dataset_vars
         self.transform_input = transform_input
         self.transform_output = transform_output
         self.preload = preload or extension == 'builtin'
+        self.default_transform = D.DefaultTransform(extension)
+        self.len_dataset = len(self.dataset_filenames)
 
         if self.preload:
             self.dataset = [self.data_reader(f, **self.dataset_vars) for f in self.dataset_filenames]
-            self.dataset = self.dataset[0] if extension == 'builtin' else dataset
+            self.dataset = self.dataset[0] if extension == 'builtin' else self.dataset
+            self.len_dataset = len(self.dataset)
 
     def __len__(self):
-        return len(self.dataset_filenames)
+        return self.len_dataset
 
     def __getitem__(self, idx):
         if self.preload:
@@ -61,9 +51,13 @@ class CustomDataset(Dataset):
 
         if self.transform_input:
             data = self.transform_input(data)
+        else:
+            data = self.default_transform.transform_data(data)
 
         if self.transform_output:
             label = self.transform_output(label)
+        else:
+            label = self.default_transform.transform_label(label)
 
         return data, label
 
@@ -89,18 +83,30 @@ class Dataset:
 
 
 if __name__ == '__main__':
-    dataset_vars = dict(name='cifar10')
-    dataset = Dataset('/home/enmartz/Downloads',
-                      'builtin',
-                      dataset_vars=dataset_vars,
-                      transform_input=None,
-                      transform_output=None,
-                      preload=False,
-                      use_loader=True,
-                      batch_size=32,
-                      shuffle=False,
-                      num_workers=0)
-    print(dataset.train_loader)
-    for data, label in dataset.train_loader:
-        print(data, label)
-        break
+    dataset_vars = dict(name='cifar10', train=True, download=True)
+    train_dataset = Dataset('/home/enmartz/Downloads',
+                            'builtin',
+                            dataset_vars=dataset_vars,
+                            transform_input=None,
+                            transform_output=None,
+                            preload=False,
+                            use_loader=True,
+                            batch_size=32,
+                            shuffle=False,
+                            num_workers=0)
+
+    # plot 3 x 3 images
+
+    data, label = next(iter(train_dataset.train_loader))
+
+    plt.figure(figsize=(5, 5))
+    plt.suptitle('CIFAR10 dataset Samples')
+
+    for i in range(9):
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(data[i].permute(1, 2, 0).cpu().numpy())
+        plt.title(label[i].cpu().numpy())
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
--- colibri/data/utils.py
+++ colibri/data/utils.py
@@ -1,6 +1,7 @@
 import os
 
 import torchvision
+from torchvision import transforms
 from PIL import Image
 
 BUILTIN_DATASETS = {
@@ -10,6 +11,34 @@ BUILTIN_DATASETS = {
     'cifar100': torchvision.datasets.CIFAR100
 }
 
+
+class DefaultTransform:
+    def __init__(self, extension):
+        if extension == 'builtin':
+            self.transform_data = transforms.ToTensor()
+            self.transform_label = transforms.Lambda(lambda x: x)
+
+        else:
+            raise NotImplementedError
+
+    def transform_data(self, data):
+        return self.transform_data(data)
+
+    def transform_label(self, label):
+        return self.transform_label(label)
+
+
+def get_filenames(path, extension, **kwargs):
+    if extension == 'builtin':
+        name = kwargs['name']
+        path = os.path.join(path, name)
+        os.makedirs(path, exist_ok=True)
+
+        return [path]
+
+    else:
+        """Return a list with the filenames of the files in the given path."""
+        return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 def load_builtin(path, **kwargs):
     name = kwargs['name']
