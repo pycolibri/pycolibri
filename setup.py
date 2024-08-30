369a12e667f9dded89e8f13275a4df27e55ccd6e
David Santiago Morales Norato <d.s.norato@gmail.com>
2024-08-30 08:43:03 -0500

    Update setup.py
    updated requests requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='colibri',
-    install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision', 'matplotlib', 'h5py', 'torch_dct'],
+    install_requires=['torch', 'torchmetrics', 'tqdm', 'torchvision', 'matplotlib', 'h5py', 'torch_dct', 'requests'],
     extras_require={'doc': ['sphinx', 'furo', 'autodocsumm', 'sphinx_gallery'],
                     'test': ['pytest']}
 )
