95dda0759705db5e5e5197d75c53d297030e7387
David Santiago Morales Norato <d.s.norato@gmail.com>
2024-09-07 14:16:59 -0500

    improved before pr file

--- before_pr.sh
+++ before_pr.sh
@@ -2,11 +2,12 @@
 if [[ "$1" == "--docs" ]]; then
     rm -r docs/build docs/source/auto_examples docs/source/gen_modules docs/source/stubs docs/source/sg_execution_times.rst
     make html -C docs/
+    open docs/build/html/index.html
 elif [[ "$1" == "--tests" ]]; then
     pytest -v -s
 else
     rm -r docs/build docs/source/auto_examples docs/source/gen_modules docs/source/stubs docs/source/sg_execution_times.rst
     make html -C docs/
-    pytest -v -s
     open docs/build/html/index.html
+    pytest -v -s
 fi
