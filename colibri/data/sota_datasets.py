6a1df95bd4572624230dd16cfddc605c786fbdad
Enmartz <Enmartz@hotmail.com>
2025-02-17 11:12:12 -0500

    Update cave url (#173)

--- colibri/data/sota_datasets.py
+++ colibri/data/sota_datasets.py
@@ -17,7 +17,7 @@ class CaveDataset():
     """
 
     def __init__(self, path: str, download: bool = True,
-                 url: str = 'https://www.cs.columbia.edu/CAVE/databases/multispectral/zip/complete_ms_data.zip'):
+                 url: str = 'https://cave.cs.columbia.edu/old/databases/multispectral/zip/complete_ms_data.zip'):
         self.url = url
         self.tmp_name = 'cave_dataset'
         self.path = os.path.join(path, self.tmp_name)
