ec45cc3919ec1530b95058c4082dba5ff7738511
Brayan Monroy <brayansneider22@gmail.com>
2025-04-30 11:39:34 -0500

    include PnP ADMM for demo fast cassi

--- examples/demo_fast_sdcassi.py
+++ examples/demo_fast_sdcassi.py
@@ -44,7 +44,7 @@ else:
 # -----------------------------------------------
 M = 256
 N = M
-L = 1
+L = 3
 sample_index = 1
 
 # %%
@@ -54,7 +54,10 @@ sample_index = 1
 dataset = CustomDataset('cave', path='data')
 sample = dataset[sample_index]
 x = sample['output'].numpy()
+x = x[::-1, ...]
 x = torch.from_numpy(resize(x[::int(x.shape[0] / L) + 1], [L, M, N]))[None, ...].float()
+
+x = x / x.max()  # Normalize the image to [0, 1]
 
 print(x.shape)
 
@@ -74,11 +77,10 @@ ca = sd_cassi.learnable_optics[0]
 # -----------------------------------------------
 recons_fn = L2L2SolverSDCASSI(y, sd_cassi)
 xtilde    = torch.zeros_like(x, device=x.device) # initial guess
-rho       = 1.0  # regularization parameter
+rho       = 1e-4  # regularization parameter
 
 # solution with the solver
-# recons_img  = recons_fn.solve(xtilde, rho)
-recons_img = y / ca
+recons_img  = recons_fn.solve(xtilde, rho)
 
 # solution with the manual inversion
 [Qinv, rhoIQ] = recons_fn.ComputeQinv(rho)
@@ -86,24 +88,69 @@ recons_img2 = recons_fn.IMVMS(sd_cassi(y, type_calculation="backward"), Q=Qinv)
 
 error = torch.norm(recons_img - recons_img2)  # compute the error
 
-fig, ax = plt.subplots(2, 2, figsize=(10, 10))
-plt.suptitle(f'Inversion error norm2(recon_img1 - recon_img2): {error:.4e}', fontsize=16)
+fig, ax = plt.subplots(2, 2, figsize=(6, 6))
+plt.suptitle(f'Inversion error norm2(recon_img1 - recon_img2): {error:.4e}', fontsize=12)
 
-ax[0, 0].imshow(x[0, 0] / x.max() )
+ax[0, 0].imshow(x[0].permute(1, 2, 0) / x.max() )
 ax[0, 0].set_title("Original Image")
 ax[0, 0].axis("off")
 
-ax[0, 1].imshow(y[0, 0])
+ax[0, 1].imshow(y[0].permute(1, 2, 0))
 ax[0, 1].set_title("Measurement")
 ax[0, 1].axis("off")
 
-ax[1, 0].imshow(recons_img[0, 0] / recons_img.max() )
+ax[1, 0].imshow(recons_img[0].permute(1, 2, 0) / recons_img.max() )
 ax[1, 0].set_title("Reconstructed Image 1")
 ax[1, 0].axis("off")
 
-ax[1, 1].imshow(recons_img2[0, 0]/ recons_img2.max() )
+ax[1, 1].imshow(recons_img2[0].permute(1, 2, 0) / recons_img2.max() )
 ax[1, 1].set_title("Reconstructed Image 2")
 ax[1, 1].axis("off")
 
 plt.tight_layout()
+plt.show()
+
+from colibri.recovery.terms.fidelity import L2
+from colibri.recovery.terms.prior import Sparsity
+from colibri.recovery.pnp import PnP_ADMM
+
+
+fidelity = L2()
+prior = Sparsity(basis="dct")
+
+algo_params = {
+    'max_iters': 10,
+    '_lambda': 0.1,
+    'rho': 0.01,
+    'alpha': 1e-4,
+}
+
+pnp = PnP_ADMM(sd_cassi, fidelity, prior, **algo_params)
+
+x0 = recons_img
+x_hat = pnp(y, x0=x0,  verbose=False).detach()
+
+
+fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+
+plt.subplot(1, 4, 1)
+plt.title("Measurement")
+plt.imshow(y[0].permute(1, 2, 0))
+plt.axis("off")
+
+plt.subplot(1, 4, 2)
+plt.title("Fast Inversion")
+plt.imshow(recons_img[0].permute(1, 2, 0) / recons_img.max())
+plt.axis("off")
+
+plt.subplot(1, 4, 3)
+plt.title("PnP ADMM")
+plt.imshow(x_hat[0].permute(1, 2, 0) / x_hat.max())
+plt.axis("off")
+
+plt.subplot(1, 4, 4)
+plt.title("Reference")
+plt.imshow(x[0].permute(1, 2, 0) / x.max())
+plt.axis("off")
+
 plt.show()
