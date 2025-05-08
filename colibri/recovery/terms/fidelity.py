f4ace815aa855a1e01dcd6e9e697076c613a0963
Román Alejandro Jácome Carrascal <roaljaca@gmail.com>
2025-05-08 14:57:48 -0500

    L1

--- colibri/recovery/terms/fidelity.py
+++ colibri/recovery/terms/fidelity.py
@@ -64,7 +64,7 @@ class L1(torch.nn.Module):
         super(L1, self).__init__()
 
     def forward(self, x, y, H=None):
-        r""" Computes the L2 fidelity term.
+        r""" Computes the L1 fidelity term.
 
         Args:
             x (torch.Tensor): The image to be reconstructed.
@@ -72,7 +72,7 @@ class L1(torch.nn.Module):
             H (function, optional): The forward model. Defaults to None.
 
         Returns:
-            torch.Tensor: The L2 fidelity term.
+            torch.Tensor: The L1 fidelity term.
         """
         r = H(x) - y
         r = r.reshape(r.shape[0],-1)
@@ -81,10 +81,10 @@ class L1(torch.nn.Module):
     
     def grad(self, x, y, H=None, transform=None):
         r'''
-        Compute the gradient of the L2 fidelity term.
+        Compute the gradient of the L1 fidelity term.
 
         .. math::
-            \nabla f(\mathbf{x}) = \nabla \frac{1}{2}||\forwardLinear(\mathbf{x}) - \mathbf{y}||^2_2
+            \nabla f(\mathbf{x}) = \nabla \frac{1}{2}||\forwardLinear(\mathbf{x}) - \mathbf{y}||_1
 
         Args:
             x (torch.Tensor): Input tensor.
